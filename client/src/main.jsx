1bbf2400c0adca702c828e935cd33b16220d8915
vinaapatricia <vinaapatricia@gmail.com>
2023-12-07 13:46:02 +0700

    add google auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import { persistor, store } from './redux/store.js';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
