1a38ee24d6ecd5932842dfd2f291ad9f2cef8aae
vinaapatricia <vinaapatricia@gmail.com>
2023-12-08 10:14:27 +0700

    add cors package

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 dotenv.config();
 
 mongoose
