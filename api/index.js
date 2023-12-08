f856e8a2b05e45e8517a847e900e4849784a7cb8
vinaapatricia <vinaapatricia@gmail.com>
2023-12-08 11:35:46 +0700

    update

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: ["https://mern-preloved-vinaapatricia.vercel.app/"],
+    origin: ["https://mern-preloved-api.vercel.app"],
     methods: ["POST","GET"],
     credentials: true
   }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home"
 import Profile from "./pages/Profile"
 import About from "./pages/About"
 import SignUp from "./pages/SignUp"
-import SignIn from "./pages/signIn"
+import SignIn from "./pages/SignIn"
 import Header from "./components/Header"
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from "./pages/CreateListing"
