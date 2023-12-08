71b6db1f783daf1fa9ca2f83980ecee5cab56dc8
vinaapatricia <vinaapatricia@gmail.com>
2023-12-08 11:24:40 +0700

    add api link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-undef */
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
@@ -8,8 +9,17 @@ import SignIn from "./pages/signIn"
 import Header from "./components/Header"
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from "./pages/CreateListing"
+// import { axios } from "axios";
+import axios from '/node_modules/.vite/deps/axios.js?v=b046a8de';
+
 
 export default function App() {
+
+axios.defaults.withCredentials = true;
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  axios.post('https://mern-preloved-api.vercel.app/api/auth/signup').then(result => console.log(result)).catch(err => console.log(err))
+}
   return (
   <BrowserRouter>
   <Header />
